d83fbb76b9c5988f4a0b79f3058b688209eb2c81
lacock32-a11y <lacock32@gmail.com>
2026-03-02 18:09:19 +0000

    Changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: "https://deepdrop-backend2.onrender.com/api", // backend url
+ baseURL: "https://deepdrop-backend2-1.onrender.com", // backend url
 });
 
 // attach token automatically
